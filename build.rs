712065a09707610f51e993fadce002da3b904572
Piet Geursen <pietgeursen@gmail.com>
2018-10-12 11:48:48 +1300

    Tidy up build.rs

--- build.rs
+++ build.rs
@@ -1,19 +1,16 @@
-use std::process::Command;
+use std::process::{Command, ExitStatus};
 use std::env;
 
 fn main() {
-
-    env::var("SODIUM_STATIC").expect("SODIUM_STATIC was not set");
     env::var("SODIUM_STATIC").and_then(|_|{
         env::var("SODIUM_LIB_DIR").expect("SODIUM_LIB_DIR was not set");
         env::var("SODIUM_INSTALL_DIR").expect("SODIUM_INSTALL_DIR was not set");
-
-        //Command::new("make").args(&["clean"])
-        //    .status().expect("could not clean libsodium");
+        
+        Command::new("make").args(&["clean"])
+            .status(); //Clean might fail if the project wasn't configured or was cleaned already. Just ignore result.
         Command::new("make").args(&["libsodium", "-j4"])
-                       .status().expect("could not make libsodium");
+            .status().expect("could not make libsodium");
         Ok(0)
-    }).unwrap();
-
-
+    })
+    .unwrap();
 }
