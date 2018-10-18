5e109772ab3a776ee8e6ec4f134e7d4127a83a2e
Piet Geursen <pietgeursen@gmail.com>
2018-10-18 16:46:11 +1300

    Fix annoying build order error with makefile. Code is eck.

--- build.rs
+++ build.rs
@@ -4,15 +4,20 @@ use std::env;
 fn main() {
     match env::var("SODIUM_STATIC") {
         Ok(_) => {
-        env::var("SODIUM_LIB_DIR").expect("SODIUM_LIB_DIR was not set");
-        env::var("SODIUM_INSTALL_DIR").expect("SODIUM_INSTALL_DIR was not set");
-        
-        //TODO: don't want to clean every time.
-        Command::new("make").args(&["clean"])
-            .status(); //Clean might fail if the project wasn't configured or was cleaned already. Just ignore result.
-        Command::new("make").args(&["libsodium", "-j4"])
-            .status().expect("could not make libsodium");
+            env::var("SODIUM_LIB_DIR").expect("SODIUM_LIB_DIR was not set");
+            env::var("SODIUM_INSTALL_DIR").expect("SODIUM_INSTALL_DIR was not set");
+
+            //TODO: don't want to clean every time.
+            Command::new("make").args(&["clean"])
+                .status() //Clean might fail if the project wasn't configured or was cleaned already. Just ignore result.
+                .map(|_|())
+                .map_err(|_|())
+                .unwrap();
+
+            Command::new("make").args(&["libsodium", "-j4"])
+                .status()
+                .expect("could not make libsodium");
         },
         _ => ()
-    }
+    };
 }
